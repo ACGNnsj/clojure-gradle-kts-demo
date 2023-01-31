b8a00cd3a6ebe8246774127d0422edab490a2ee3
ACGNnsj <ootopoo@vip.qq.com>
2023-01-31 23:04:44 +0800

    0

---
+++ build.gradle.kts
@@ -0,0 +1,74 @@
+import dev.clojurephant.plugin.clojure.tasks.ClojureCompile
+
+//import clojuresque.tasks.ClojureSourceSet
+//import groovy.xml.dom.DOMCategory.attributes
+
+/*buildscript {
+    repositories {
+        maven { url = uri("https://clojars.org/repo") }
+        mavenCentral()
+    }
+    dependencies {
+        classpath("clojuresque:clojuresque:1.7.0")
+    }
+}*/
+//apply(plugin = "clojure")
+
+plugins {
+    id("dev.clojurephant.clojure") version "0.7.0"
+//    id("java")
+//    id("clojure")
+//    clojure("1.7.0")
+    id("application")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://clojars.org/repo") }
+//    dependencies {
+//        classpath("com.android.tools.build:gradle:4.1.2")
+//        classpath("clojuresque:clojuresque:1.7.0")
+//        classpath("io.github.gradle-clojure:gradle-clojure-plugin:0.5.0-alpha.3")
+//    }
+}
+dependencies {
+    implementation("org.clojure:clojure:1.11.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+clojure {
+    builds.maybeCreate("main").aotAll()
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+var count: Int = 0
+tasks.withType<ClojureCompile> {
+    println("${count++}: compileClojure")
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            mapOf(
+                "Main-Class" to "main.hello",
+                "Implementation-Title" to "clojure-lib",
+                "Implementation-Version" to archiveVersion
+            )
+        )
+    }
+}
+//sourceSets {
+//    main {
+//        withConvention(ClojureSourceSet::class) {
+//            clojure.srcDir("src/main/clojure")
+//        }
+//    }
+//}
+application {
+    mainClassName = "main.hello"
+}
