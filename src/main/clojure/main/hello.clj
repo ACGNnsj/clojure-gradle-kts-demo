b905ef81e9487d8028a9de1bb17fd5a786299554
ACGNnsj <ootopoo@vip.qq.com>
2023-02-01 11:10:44 +0800

    2

--- src/main/clojure/main/hello.clj
+++ src/main/clojure/main/hello.clj
@@ -1,10 +1,11 @@
 (ns main.hello
   (:gen-class)
-  (:import (org.example.api Calculation)))
+  (:require [wrapper.twitter :refer [add]]))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!")
-  (println (Calculation/add 1 2))
+  (println (add 4 5))
   )
---
+++ src/main/clojure/wrapper/twitter.clj
@@ -0,0 +1,6 @@
+(ns wrapper.twitter
+  (:import (org.example.api Calculation)))
+(defn add
+  "docstring"
+  [x y] (Calculation/add x y)
+  )
