2a031e0d0d355c0b8a8e9c4fae3a12bd76fff388
ACGNnsj <ootopoo@vip.qq.com>
2023-02-02 14:11:56 +0800

    Try to trigger workflow
    Signed-off-by: ACGNnsj <ootopoo@vip.qq.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,22 @@ println(signingKey.contains("lIYEY9oMWxYJKwYBBAHaRw8BAQdAZdyZAKkdZM"))
 println(signingKey.contains("\n"))
 println(signingKey.contains("\r"))
 println(signingKey.contains("\\n"))
+println(
+    ("lIYEY9oMWxYJKwYBBAHaRw8BAQdAZdyZAKkdZMU98tAGq+iLAYbzRTK0JfMK6hvU\n" +
+            "4RTpoSn+BwMCxM+FZQAPqS/OsdBZ5Ejx65c2VLiiF0hqsLQ+OofsMgg3cQlLHjZ8\n" +
+            "h8JuXTV4u+IXY2S8ntSk+tRT2pRaebn/Hb9g35/DC92Y5plyU/Py97QaQUNHTU4g\n" +
+            "PG9vdG9wb29AdmlwLnFxLmNvbT6ImQQTFgoAQRYhBG5uERqOHBbLegVVO/uIXpIt\n" +
+            "QioWBQJj2gxbAhsDBQkDw4/lBQsJCAcCAiICBhUKCQgLAgQWAgMBAh4HAheAAAoJ\n" +
+            "EPuIXpItQioWF2sA/3AHqhXkM62Hpp//nnKHcqQYXO+d81jZgNWkO5H9eVYFAQCs\n" +
+            "533eK3zwvmRHhaKhWw05Dyk20F1VWX/i9zhKKftTApyLBGPaDFsSCisGAQQBl1UB\n" +
+            "BQEBB0DotNIkgTjvXtw68Fs02fdXQrL7f8fRqZayCFDM0ABuBQMBCAf+BwMCU1DC\n" +
+            "I/soDOTOtAU9wb/CyOM3CG3My9n/609F+75t/sVR6kYPh6+ztPtqSrIZC/63Dh7T\n" +
+            "f83upMsIOPeUOKA2eP6t7xNNe6ep4kfXXsXjL4h+BBgWCgAmFiEEbm4RGo4cFst6\n" +
+            "BVU7+4heki1CKhYFAmPaDFsCGwwFCQPDj+UACgkQ+4heki1CKhZU9AD/axprT9zb\n" +
+            "icv91IGUJiwo5ZSLsvDjIr2xvImrlm0LBkIBAMiVGc1ZwQ7lxHTXx4F/eCnukTD7\n" +
+            "9ZBWinrS7vLAuREN\n" +
+            "=so4m") == signingKey
+)
 
 group = "io.github.acgnnsj"
 version = "1.0.0"
